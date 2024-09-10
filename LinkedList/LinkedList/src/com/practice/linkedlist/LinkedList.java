0afa1211cdf390f2f53ae5ee0c96efd90a2db8b4
Ashutosh Kumar Singh <ashutoshkmrsngh06@gmail.com>
2024-09-10 23:11:47 +0530

    Problem of the day 10 Sep 2024

---
+++ LinkedList/LinkedList/src/com/practice/linkedlist/LinkedList.java
@@ -0,0 +1,11 @@
+package com.practice.linkedlist;
+
+@Getter
+@Setter
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+}
---
+++ LinkedList/LinkedList/src/com/practice/linkedlist/Node.java
@@ -0,0 +1,11 @@
+package com.practice.linkedlist;
+
+public class Node {
+
+    int value;
+    Node next;
+
+    Node(int value){
+        this.value = value;
+    }
+}
